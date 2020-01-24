f981e11c903735862cbdc88dd871cecc4d97b39a
Penghua <kids5346@gmail.com>
2020-01-24 14:40:11 +0800

    build simple test: use getByLabelText method

---
+++ __test__/dom.test.js
@@ -0,0 +1,25 @@
+// const  sum  = require('../dom');
+
+const { getByLabelText, screen } = require('@testing-library/dom');
+
+function domExample() {
+  // return dom => here is div 
+  const div = document.createElement('DIV');
+  const exampleHTML = `
+    <label for="example">Example</label>
+    <input id="example" />
+  `;
+  div.innerHTML = exampleHTML;
+  return div;
+}
+
+test('check input id equal to example.', () => {
+  // execute domExample function to obtain div.
+  const container = domExample();
+  // use getByLabelText method to query text: Example.
+  // (Note: Here label and input need to work together.)
+  // see the reason in github : https://github.com/testing-library/dom-testing-library/blob/master/src/__tests__/element-queries.js#L147
+  const exampleInput = getByLabelText(container, 'Example');
+  // use Jest matchers to test.
+  expect(exampleInput.id).toBe('example');
+})
